fb1eaa6d248188371e253e8a49e969f645d4222c
Wyatt <whclausen0528@gmail.com>
2018-01-13 09:30:28 -0600

    the inverse of a mean function

---
+++ R2.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class R2 {
+public static Scanner R = new Scanner(System.in);
+public static void main(String[] arg){
+	int x = R.nextInt();
+	int y = R.nextInt();
+	System.out.println(2 * y - x);
+}
+}
